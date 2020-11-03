16da770d58e1181dc1dacf0f441e86405d2b40bf
Kelly <guojiyou40020@Hotmail.com>
2020-11-03 23:12:40 +0800

    added postId in data struct

--- app/service/mock-get-posts.ts
+++ app/service/mock-get-posts.ts
@@ -1,15 +1,18 @@
 import { Service } from "egg";
+import { nanoid } from 'nanoid';
 
 class MockGetPosts extends Service {
   public async getPosts() {
     const data = [
       {
+        postId: nanoid(15), 
         title: "Test title",
         tags: "tag1 tag2",
         contentPreview: "preview preview",
         dateTime: 1212300124,
       },
       {
+        postId: nanoid(15), 
         title: "Test title",
         tags: "tag1 tag2",
         contentPreview: "preview preview",
